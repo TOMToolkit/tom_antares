9df0ee3698abd415606773b3e8f95e59dc414c05
Austin Riba <austin@m51.io>
2026-06-03 13:52:40 -0700

    Update tests to use PhotometryReducedDatum fields

--- tom_antares/tests/tests.py
+++ tom_antares/tests/tests.py
@@ -185,13 +185,13 @@ class TestAntaresDataservice(TestCase):
 
     def test_create_reduced_datums_from_query(self):
         reduced_data = self.antares_query.create_reduced_datums_from_query(self.test_target, lightcurve_data)
-        keys_list = [('ant_mag', 'magnitude'),
-                     ('ant_magerr', 'error'),
+        keys_list = [('ant_mag', 'brightness'),
+                     ('ant_magerr', 'brightness_error'),
                      ('ant_maglim', 'limit'),
-                     ('ant_passband', 'filter')]
+                     ('ant_passband', 'bandpass')]
         for i, reduced_datum in enumerate(reduced_data):
             for key in keys_list:
                 if lightcurve_data[i][key[0]]:
-                    self.assertIn(key[1], reduced_datum.value)
+                    self.assertEqual(getattr(reduced_datum, key[1]), lightcurve_data[i][key[0]])
                 else:
-                    self.assertNotIn(key[1], reduced_datum.value)
+                    self.assertIsNone(getattr(reduced_datum, key[1]))
